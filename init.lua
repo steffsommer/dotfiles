da34c20dc7609dec54de1f6ce57426006c29165c
Stefan Sommer <stefan.sommer@diovision.net>
2021-03-30 14:08:54 +0200

    Added first keybindings for init.lua

--- init.lua
+++ init.lua
@@ -33,3 +33,14 @@ vim.o.undofile = true
 vim.o.undodir='/tmp'
 vim.o.mouse = 'a'
 
+-- ~~~~~~~~ +=------------------------------=+"
+-- =------------------------------=+ KEY MAPS +=------------------------------=+"
+-- +=------------------------------=+ ~~~~~~~~ +=------------------------------=+"
+
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
+
+vim.g.mapleader = ','
+
