16efb0944b48c6c649ac5f3072d746ec0693d6ec
Stefan Sommer <stefan.sommer@diovision.net>
2021-03-30 13:56:04 +0200

    Added init.lua with general neovim config

---
+++ init.lua
@@ -0,0 +1,35 @@
+--    _____   _____________    ________  ___
+--   /  _/ | / /  _/_  __/ |  / /  _/  |/  /
+--   / //  |/ // /  / /  | | / // // /|_/ / 
+-- _/ // /|  // /  / /   | |/ // // /  / /  
+-- ___/_/ |_/___/ /_(_)  |___/___/_/  /_/   
+
+-- author: somsky
+
+-- +=--------------------------=+ ~~~~~~~~~~~~~~~~ +=--------------------------=+ --
+-- +=--------------------------=+ GENERAL SETTINGS +=--------------------------=+ --
+-- +=--------------------------=+ ~~~~~~~~~~~~~~~~ +=--------------------------=+ --
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.hlsearch = false
+vim.o.scrolloff=5
+vim.o.sidescrolloff=15
+vim.o.expandtab = true
+vim.o.tabstop=4
+vim.o.shiftwidth=4
+vim.o.smarttab = true
+vim.o.wrap = false
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.swapfile = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.autoread = true
+vim.o.undofile = true
+vim.o.undodir='/tmp'
+vim.o.mouse = 'a'
+
