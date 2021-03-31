dfbd5f92b8bbd0f41384b6b2f4a4ae6470934fbc
Stefan Sommer <stefan.sommer@diovision.net>
2021-03-31 12:18:52 +0200

    Further init.lua updates

--- init.lua
+++ init.lua
@@ -10,37 +10,51 @@
 -- +=--------------------------=+ GENERAL SETTINGS +=--------------------------=+ --
 -- +=--------------------------=+ ~~~~~~~~~~~~~~~~ +=--------------------------=+ --
 
-vim.wo.number = true
+-- window options
+vim.wo.wrap           = false
+vim.wo.scrolloff      = 5
+vim.wo.sidescrolloff  = 15
+vim.wo.number         = true
 vim.wo.relativenumber = true
-vim.o.hlsearch = false
-vim.o.scrolloff=5
-vim.o.sidescrolloff=15
-vim.o.expandtab = true
-vim.o.tabstop=4
-vim.o.shiftwidth=4
-vim.o.smarttab = true
-vim.o.wrap = false
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.swapfile = false
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
-vim.o.autoread = true
-vim.o.undofile = true
-vim.o.undodir='/tmp'
-vim.o.mouse = 'a'
 
--- ~~~~~~~~ +=------------------------------=+"
--- =------------------------------=+ KEY MAPS +=------------------------------=+"
+-- general options
+vim.o.hlsearch    = false
+vim.o.expandtab   = true
+vim.o.tabstop     = 4
+vim.o.shiftwidth  = 4
+vim.o.smarttab    = true
+vim.o.autoindent  = true
+vim.o.smartindent = true
+vim.o.splitright  = true
+vim.o.splitbelow  = true
+vim.o.swapfile    = false
+vim.o.ignorecase  = true
+vim.o.smartcase   = true
+vim.o.incsearch   = true
+vim.o.autoread    = true
+vim.o.undofile    = true
+vim.o.undodir     ='/tmp'
+vim.o.mouse 	  = 'a'
+vim.o.clipboard   = '+=a' -- use system clipboard
+
+-- vim.api.nvim_exec([[autocmd FocusGained,BufEnter * :checktime]])
+
 -- +=------------------------------=+ ~~~~~~~~ +=------------------------------=+"
+-- =-------------------------------=+ KEY MAPS +=------------------------------=+"
+-- +=------------------------------=+ ~~~~~~~~ +=------------------------------=+"
+
+vim.g.mapleader = ','
+
+vim.api.nvim_set_keymap('n', '<leader>s' , ':update<CR>', { noremap = true })
 
 vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
 
-vim.g.mapleader = ','
+
+vim.api.nvim_set_keymap('n', '<leader>+', ':vertical resize +20<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>-', ':vertical resize -20<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>rc', ':luafile ~/.config/nvim/init.lua<CR>', { noremap = true })
 
