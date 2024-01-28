534c987706a9e99675375d219966430d85d697f6
Stefan Sommer <stefan.sommer.per@gmail.com>
2024-01-28 22:13:38 +0100

    updated keybinds, dashboard and changed neotree layout

---
+++ lazyvim/lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "float",
+        popup = {
+          size = {
+            height = "80%",
+            width = "50%",
+          },
+        },
+      },
+    },
+  },
+}
