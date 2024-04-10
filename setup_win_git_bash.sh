3b86054fee509bdd04e44bbb271e3777de3fe41a
Stefan Sommer <stefan.sommer@intive.com>
2024-04-10 15:16:28 +0200

    updated windows setup

--- setup_win_git_bash.sh
+++ setup_win_git_bash.sh
@@ -1,41 +1,64 @@
 #!/usr/bin/env bash
 # Caveats: Currently only works for directories, because mklink /D is used
 
-if [[ $(sfc 2>&1 | tr -d '\0') =~ SCANNOW ]]; then
-	IS_ADMIN=true
-else
-	IS_ADMIN=false
-fi
-if [[ $IS_ADMIN = false ]]; then
-	echo "This script must be run from a Git bash that was executed as Administrator"
-	exit 1
+
+log_info() {
+  echo "[INFO] $1"
+}
+
+error_exit() {
+  echo "[ERROR] $1"
+  exit 1
+}
+
+if ! [[ $(sfc 2>&1 | tr -d '\0') =~ SCANNOW ]]; then
+  error_exit  "This script must be run from a Git bash that was executed as Administrator"
 fi
 
-read -p "Enter the username the dotfiles should be set up for: " DOTFILES_USER
-echo $DOTFILES_USER
+read -p "Enter the username the dotfiles should be set up for: " user
 
-## Links ##
-# Links must be the windows absolute paths, because mklink will be invoked using cmd
+user_home_dir="/c/Users/$user"
+if ! [ -d $user_home_dir ]
+then
+  error_exit "Home directory $user_home_dir for user $user does not exist"
+fi
+
 declare -A filesToSymlink
 filesToSymlink=(
-  ["nvim"]="C:\\Users\\$DOTFILES_USER\\AppData\\Local\\nvim"
+  ["nvim"]="/c/Users/$user/AppData/Local/nvim"
 )
 
 dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dotfiles_dir_win_path=$(cygpath $dotfiles_dir)
 
 # symlink all configured files
-echo "[INFO] Symlinking dotfiles ..."
+log_info "Symlinking dotfiles ..."
+
 for file in "${!filesToSymlink[@]}"
 do
-  origin_file_path=$dotfiles_dir_win_path/$file
-  link_target_path=${filesToSymlink[$file]}
-  echo "Linking $origin_file_path to $link_target_path"
+  dotfile_linux_path=$dotfiles_dir/$file
+  dotfile_windows_path=$(cygpath -d $dotfile_linux_path)
 
-  git_bash_path=$(cygpath ${filesToSymlink[$file]})
-  mkdir -p $(dirname git_bash_path)
+  dotfile_linux_target_path=${filesToSymlink[$file]}
+  dotfile_windows_target_path=$(cygpath -d $dotfile_linux_target_path)
 
-  cmd "/C mklink /D $link_target_path $origin_file_path"
+  log_info "Linking $dotfile_linux_path to $dotfile_linux_target_path"
+
+  mkdir -p $(dirname $dotfile_linux_target_path)
+  if test -f $dotfile_linux_path
+  then
+    rm -rf $dotfile_linux_path
+    cmd "/C mklink /D $dotfile_windows_target_path $dotfile_windows_path"
+  elif test -d $dotfile_linux_path
+  then
+    rm -rf $dotfile_linux_path
+    echo $dotfile_windows_target_path 
+    echo $dotfile_windows_path
+    cmd "/C mklink $dotfile_windows_target_path $dotfile_windows_path"
+  else
+    error_exit "$dotfile_linux_path does not exist and therefore cannot be linked. Exiting..."
+  fi
+
+
 done
 
-echo "[INFO] Done."
+log_info 'Done'
