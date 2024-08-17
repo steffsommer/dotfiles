dbfbdbfd507efa72e163d71753af648d1c92c042
Stefan Sommer <stefan.sommer.per@gmail.com>
2024-08-17 13:19:46 +0200

    support execution of java tests

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,131 +1,339 @@
+-- This is the same as in lspconfig.server_configurations.jdtls, but avoids
+-- needing to require that when this module loads.
+local java_filetypes = { "java" }
+
+-- Utility function to extend or override a config table, similar to the way
+-- that Plugin.opts works.
+---@param config table
+---@param custom function | table | nil
+local function extend_or_override(config, custom, ...)
+	if type(custom) == "function" then
+		config = custom(config, ...) or config
+	elseif custom then
+		config = vim.tbl_deep_extend("force", config, custom) --[[@as table]]
+	end
+	return config
+end
+
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup({
-        ui = {
-          border = "rounded",
-          height = 0.8,
-        },
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-      ensure_installed = {
-        "tsserver",
-        "lua_ls",
-        "angularls",
-        "svelte",
-        "html",
-      },
-      handlers = {
-        ["angularls"] = function()
-          local lspconfig = require("lspconfig")
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
-          local util = require("lspconfig.util")
-          lspconfig.angularls.setup({
-            capabilities = capabilities,
-            root_dir = util.root_pattern("angular.json", "project.json"),
-          })
-        end,
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-jdtls",
-    lazy = false,
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    opts = {
-      capabilities = {
-        workspace = {
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
-        },
-      },
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"java-debug-adapter",
+				"java-test",
+			},
+		},
+		config = function()
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+					height = 0.8,
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = {
+				"tsserver",
+				"lua_ls",
+				"angularls",
+				"svelte",
+				"html",
+				"jdtls",
+			},
+			handlers = {
+				["angularls"] = function()
+					local lspconfig = require("lspconfig")
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					local util = require("lspconfig.util")
+					lspconfig.angularls.setup({
+						capabilities = capabilities,
+						root_dir = util.root_pattern("angular.json", "project.json"),
+					})
+				end,
+			},
+		},
+	},
+	{
+		"mfussenegger/nvim-jdtls",
+		ft = java_filetypes,
+		opts = function()
+			local mason_registry = require("mason-registry")
+			local lombok_jar = mason_registry.get_package("jdtls"):get_install_path() .. "/lombok.jar"
+			return {
+				-- How to find the root dir for a given filename. The default comes from
+				-- lspconfig which provides a function specifically for java projects.
+				root_dir = require("lspconfig.server_configurations.jdtls").default_config.root_dir,
 
-      local util = require("lspconfig.util")
+				-- How to find the project name for a given root dir.
+				project_name = function(root_dir)
+					return root_dir and vim.fs.basename(root_dir)
+				end,
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.svelte.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.angularls.setup({
-        capabilities = capabilities,
-        root_dir = util.root_pattern("angular.json", "project.json"),
-      })
+				-- Where are the config and workspace dirs for a project?
+				jdtls_config_dir = function(project_name)
+					return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
+				end,
+				jdtls_workspace_dir = function(project_name)
+					return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
+				end,
 
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+				-- How to run jdtls. This can be overridden to a full java command-line
+				-- if the Python wrapper script doesn't suffice.
+				cmd = {
+					vim.fn.exepath("jdtls"),
+					string.format("--jvm-arg=-javaagent:%s", lombok_jar),
+				},
+				full_cmd = function(opts)
+					local fname = vim.api.nvim_buf_get_name(0)
+					local root_dir = opts.root_dir(fname)
+					local project_name = opts.project_name(root_dir)
+					local cmd = vim.deepcopy(opts.cmd)
+					if project_name then
+						vim.list_extend(cmd, {
+							"-configuration",
+							opts.jdtls_config_dir(project_name),
+							"-data",
+							opts.jdtls_workspace_dir(project_name),
+						})
+					end
+					return cmd
+				end,
 
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+				-- These depend on nvim-dap, but can additionally be disabled by setting false here.
+				dap = { hotcodereplace = "auto", config_overrides = {} },
+				dap_main = {},
+				test = true,
+				settings = {
+					java = {
+						inlayHints = {
+							parameterNames = {
+								enabled = "all",
+							},
+						},
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			-- Find the extra bundles that should be passed on the jdtls command-line
+			-- if nvim-dap is enabled with java debug/test.
+			local mason_registry = require("mason-registry")
+			local bundles = {} ---@type string[]
+			local java_dbg_pkg = mason_registry.get_package("java-debug-adapter")
+			local java_dbg_path = java_dbg_pkg:get_install_path()
+			local jar_patterns = {
+				java_dbg_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+			}
+			-- java-test also depends on java-debug-adapter.
+			-- TODO: Not sure if opts.test is really needed. Only needed if required for dap config
+			if opts.test and mason_registry.is_installed("java-test") then
+				local java_test_pkg = mason_registry.get_package("java-test")
+				local java_test_path = java_test_pkg:get_install_path()
+				vim.list_extend(jar_patterns, {
+					java_test_path .. "/extension/server/*.jar",
+				})
+			end
+			for _, jar_pattern in ipairs(jar_patterns) do
+				for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), "\n")) do
+					table.insert(bundles, bundle)
+				end
+			end
 
-          vim.keymap.set("n", "<leader>cR", require("util.lsp").rename_file, opts)
+			local function attach_jdtls()
+				local fname = vim.api.nvim_buf_get_name(0)
 
-          -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-          -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-          -- vim.keymap.set("n", "<space>wl", function()
-          --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          -- end, opts)
-          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-        end,
-      })
+				-- Configuration can be augmented and overridden by opts.jdtls
+				local config = extend_or_override({
+					cmd = opts.full_cmd(opts),
+					root_dir = opts.root_dir(fname),
+					init_options = {
+						bundles = bundles,
+					},
+					settings = opts.settings,
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				}, opts.jdtls)
 
-      -- make LSP windows bordered
-      require("lspconfig.ui.windows").default_options.border = "rounded"
-    end,
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
+				-- Existing server will be reused if the root_dir matches.
+				require("jdtls").start_or_attach(config)
+				-- not need to require("jdtls.setup").add_commands(), start automatically adds commands
+			end
+
+			-- Attach the jdtls for each java buffer. HOWEVER, this plugin loads
+			-- depending on filetype, so this autocmd doesn't run for the first file.
+			-- For that, we call directly below.
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = java_filetypes,
+				callback = attach_jdtls,
+			})
+
+			-- Setup keymap and dap after the lsp is fully attached.
+			-- https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration
+			-- https://neovim.io/doc/user/lsp.html#LspAttach
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client and client.name == "jdtls" then
+						-- custom init for Java debugger
+						require("jdtls").setup_dap(opts.dap)
+						require("jdtls.dap").setup_dap_main_class_configs(opts.dap_main)
+
+						local function test_java_class()
+							require("jdtls.dap").test_class({
+								config_overrides = type(opts.test) ~= "boolean" and opts.test.config_overrides or nil,
+							})
+						end
+
+						local keymap_opts = { buffer = args.buf }
+						-- custom jdtls keymaps
+						vim.keymap.set("n", "<leader>tt", test_java_class, keymap_opts)
+
+						-- Java Test require Java debugger to work
+						if opts.test and mason_registry.is_installed("java-test") then
+							-- custom keymaps for Java test runner (not yet compatible with neotest)
+							-- wk.add({
+							-- 	{
+							-- 		mode = "n",
+							-- 		buffer = args.buf,
+							-- 		{ "<leader>t", group = "test" },
+							-- 		{
+							-- 			"<leader>tt",
+							-- 			function()
+							-- 				require("jdtls.dap").test_class({
+							-- 					config_overrides = type(opts.test) ~= "boolean"
+							-- 							and opts.test.config_overrides
+							-- 						or nil,
+							-- 				})
+							-- 			end,
+							-- 			desc = "Run All Test",
+							-- 		},
+							-- 		{
+							-- 			"<leader>tr",
+							-- 			function()
+							-- 				require("jdtls.dap").test_nearest_method({
+							-- 					config_overrides = type(opts.test) ~= "boolean"
+							-- 							and opts.test.config_overrides
+							-- 						or nil,
+							-- 				})
+							-- 			end,
+							-- 			desc = "Run Nearest Test",
+							-- 		},
+							-- 		{ "<leader>tT", require("jdtls.dap").pick_test, desc = "Run Test" },
+							-- 	},
+							-- })
+						end
+
+						-- User can set additional keymaps in opts.on_attach
+						if opts.on_attach then
+							opts.on_attach(args)
+						end
+					end
+				end,
+			})
+
+			-- Avoid race condition by calling attach the first time, since the autocmd won't fire.
+			attach_jdtls()
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {},
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		opts = {
+			capabilities = {
+				workspace = {
+					fileOperations = {
+						didRename = true,
+						willRename = true,
+					},
+				},
+			},
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local util = require("lspconfig.util")
+
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+				root_dir = util.root_pattern("angular.json", "project.json"),
+			})
+
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+					vim.keymap.set("n", "<leader>cR", require("util.lsp").rename_file, opts)
+
+					-- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+					-- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					-- vim.keymap.set("n", "<space>wl", function()
+					--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					-- end, opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+				end,
+			})
+
+			-- make LSP windows bordered
+			require("lspconfig.ui.windows").default_options.border = "rounded"
+		end,
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,37 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-          "javascript",
-          "typescript",
-          "html",
-          "css",
-          "scss",
-        }
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"regex",
+					"toml",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+			})
+		end,
+	},
 }
