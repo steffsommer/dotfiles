410c9528b38610094b35690290478fe59fa0636a
Stefan Sommer <stefan.sommer.per@gmail.com>
2024-08-28 21:33:55 +0200

    replaced neotest with vim-test

--- nvim/lua/plugins/test-execution.lua
+++ nvim/lua/plugins/test-execution.lua
@@ -1,37 +1,48 @@
 return {
-	{
-		"rcasia/neotest-java",
-		init = function()
-			local function run_test_suite()
-				require("neotest").run.run(vim.fn.expand("%"))
-			end
-
-			vim.keymap.set("n", "<leader>tt", run_test_suite)
-		end,
-		build = function()
-			vim.cmd("NeotestJava setup")
-		end,
-	},
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"rcasia/neotest-java",
-		},
-		config = function()
-      vim.keymap.set("n", "<leader>nt", function() require("neotest").summary.toggle() end)
-			---@diagnostic disable: missing-fields
-			require("neotest").setup({
-				adapters = {
-					require("neotest-java")({
-						ignore_wrapper = false, -- whether to ignore maven/gradle wrapper
-						junit_jar = nil, -- default: .local/share/nvim/neotest-java/junit-platform-console-standalone-[version].jar
-					}),
-				},
-			})
-		end,
-	},
+  {
+    "vim-test/vim-test",
+    setup = function()
+      vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>")
+      vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+    end
+  }
+  -- NeoTest is not usable at the moment. All the adapters I have looked at so far
+  -- do not work properly in actual project settings
+  --
+	-- {
+	-- 	"rcasia/neotest-java",
+	-- 	init = function()
+	-- 		local function run_test_suite()
+	-- 			require("neotest").run.run(vim.fn.expand("%"))
+	-- 		end
+	--
+	-- 		vim.keymap.set("n", "<leader>tt", run_test_suite)
+	-- 	end,
+	-- 	build = function()
+	-- 		vim.cmd("NeotestJava setup")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"nvim-neotest/neotest",
+	-- 	dependencies = {
+	-- 		"nvim-neotest/nvim-nio",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"antoinemadec/FixCursorHold.nvim",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"rcasia/neotest-java",
+	-- 	},
+	-- 	config = function()
+ --      vim.keymap.set("n", "<leader>nt", function() require("neotest").summary.toggle() end)
+	-- 		---@diagnostic disable: missing-fields
+	-- 		require("neotest").setup({
+ --        log_level = vim.log.levels.DEBUG,
+	-- 			adapters = {
+	-- 				require("neotest-java")({
+	-- 					ignore_wrapper = false, -- whether to ignore maven/gradle wrapper
+	-- 					junit_jar = nil, -- default: .local/share/nvim/neotest-java/junit-platform-console-standalone-[version].jar
+	-- 				}),
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
