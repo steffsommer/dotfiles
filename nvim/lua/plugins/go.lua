524a75ce8b740f752744ddb113ac82c9f7cd2a23
Stefan Sommer <stefan.sommer.per@gmail.com>
2024-10-02 14:45:35 +0200

    formatting go using none-ls

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -7,15 +7,6 @@ return {
   },
   config = function()
     require("go").setup()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", { clear=false }),
-      callback = function(ev)
-        local opts = { buffer = ev.buf }
-        local go_format = require("go.format")
-        vim.keymap.set("n", "<leader>ff", go_format.gofmt, opts)
-        vim.keymap.set("n", "<leader>co", go_format.goimports, opts)
-      end,
-    })
   end,
   event = { "CmdlineEnter" },
   ft = { "go", 'gomod' },
